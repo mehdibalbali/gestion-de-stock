542736521ec77f279faf8b1d4ead57a3bbef2ddc
mehdibalbali <mehdibalbali@yahoo.fr>
2018-12-31 16:32:50 +0100

    initiation de projet

---
+++ src/gestionDeStock/Main.java
@@ -0,0 +1,31 @@
+
+package gestionDeStock;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MehdiBalbali
+ */
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Gestion de Stock ");
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
