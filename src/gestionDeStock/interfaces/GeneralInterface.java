47d58438b185c4504fdc42cd9077d0feb589ac8f
mehdibalbali <mehdibalbali@yahoo.fr>
2019-01-03 01:40:36 +0100

    Update GeneralInterface.java

--- src/gestionDeStock/interfaces/GeneralInterface.java
+++ src/gestionDeStock/interfaces/GeneralInterface.java
@@ -1,12 +1,18 @@
-/**
- * 
- */
 package gestionDeStock.interfaces;
+
+import gestionDeStock.model.GeneralModel;
+import javafx.collections.ObservableList;
 
 /**
  * @author GeekDjerbien
  *
  */
 public interface GeneralInterface {
+
+	ObservableList<GeneralModel> getAll();
+	void autoId(GeneralModel m);
+	void insert(GeneralModel m);
+	void update(GeneralModel m);
+	void delete(GeneralModel m);
 
 }
